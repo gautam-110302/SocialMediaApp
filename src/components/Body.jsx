a299a827fc6fe3bf5485f04c762148d94bf3524a
gautam-110302 <gautamvr11@gmail.com>
2025-02-09 01:30:25 +0530

    initialfiles

---
+++ src/components/Body.jsx
@@ -0,0 +1,15 @@
+import Content from "./Content";
+import LeftSidebar from "./LeftSidebar";
+import RightSidebar from "./RightSidebar";
+
+const Body = () => {
+  return (
+    <>
+      <LeftSidebar></LeftSidebar>
+      <Content></Content>
+      <RightSidebar></RightSidebar>
+    </>
+  );
+};
+
+export default Body;
